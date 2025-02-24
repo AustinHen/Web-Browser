3cc6a46095e0ff43a3aa530af8e2b27560fc433f
Austin Henlotter <ahenlotter@umass.edu>
2025-02-24 18:00:59 -0500

    added function signatures

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+fn render_html(){}
+
+fn construct_dom(){}
+
+fn get_website(url: string){
+    //get dns 
+    //http / https loop
+}
+
+//gets ip address of url
+fn dns(url: string) -> u64{
+}
+//Networking
+//DNS 
+//HTTPS / HTTP
+//DOM -> render
