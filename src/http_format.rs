e652728a169919d6b816148ce4b637a7fed0974a
Austin Henlotter <ahenlotter@umass.edu>
2025-05-20 13:37:20 -0400

    added formater macro for http requests

---
+++ src/http_format.rs
@@ -0,0 +1,54 @@
+//All http messages will be gets 
+#[macro_export]
+macro_rules! http_get {
+    ($host: expr, $url: expr) => {
+        {
+            http_header!(
+                "GET", 
+                $url, 
+                "HTTP/1.1",
+                "Host", $host, 
+                "User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.8) Gecko/20091102 Firefox/3.5.5", // just some random stuff
+                "Accept", "text/html",
+                "Accept-Language", "en-us",
+                "Accept-Encoding", "deflate",
+                "Accept-Charset", "utf-8",
+                "Cache-Control", "no-cache"
+                )
+        }
+    };
+
+}
+
+
+#[macro_export]
+macro_rules! http_header {
+    ($msg_type : expr, $url : expr, $version : expr, $($name : expr, $val : expr ),*) => {
+        {
+            //TODO do better 
+            //idk what the best way to concat strings is in rust 
+            let mut temp_str = String::new();
+            temp_str.push_str($msg_type);
+            temp_str.push(' ');
+            temp_str.push_str($url);
+            temp_str.push(' ');
+            temp_str.push_str($version);
+            temp_str.push(' ');
+            temp_str.push_str("\r\n");
+
+            $(
+                temp_str.push_str($name);
+                temp_str.push(':');
+                temp_str.push(' ');
+                temp_str.push_str($val);
+                temp_str.push_str("\r\n");
+             )*
+            temp_str.push_str("\r\n");
+        
+            temp_str
+        }
+    };
+}
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod gui;
 mod htmlParser;
 mod network;
+mod http_format;
 
 fn main() {
     println!("Booting up (what a great debug message) (useful newline)\n ");
+    println!("{0}", http_get!("youtube.com", "/video.html"));
     htmlParser::test_parser();
     gui::gui_main();
 }
